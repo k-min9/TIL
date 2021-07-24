09ca195aa704ff544d8ebc0086fea448bb38bdfd
k-min9 <mingu4969@gmail.com>
2021-07-24 16:50:35 +0900

    210724 BOJ

---
+++ 00. Daily Algorithm/BOJ/BOJ11054_Gd3_가장긴_바이토닉_부분수열.py
@@ -0,0 +1,36 @@
+'''
+감상 : 아는 문제
+접근 : 가장 긴 증가하는 부분수열(LIS)과 그 reverse 버전 합치면 끝
+'''
+
+import sys
+input = sys.stdin.readline
+
+def getDP_LIS(N, seq ,dp):
+    for i in range(N):
+        for j in range(i):
+            if seq[j] < seq[i]:
+                dp[i] = max(dp[i], dp[j] + 1)
+    return dp
+
+
+# 입력
+N = int(input()) # 수열 크기
+seq = list(map(int,input().split())) #수열
+
+# DP
+dp1 = [1]*N #LIS적용
+dp2 = [1]*N #리버스 하고 LIS적용
+
+dp1 = getDP_LIS(N, seq, dp1)
+seq.reverse()
+dp2 = getDP_LIS(N, seq, dp2)
+dp2.reverse()
+
+#출력
+answer = []
+for i in range(N):
+    answer.append(dp1[i]+dp2[i])
+
+
+print(max(answer)-1)
