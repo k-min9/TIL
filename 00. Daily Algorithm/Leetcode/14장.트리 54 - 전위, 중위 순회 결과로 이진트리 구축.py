355b24516c81d574303475007ab5c0a6656fa49e
k-min9 <mingu4969@gmail.com>
2021-08-09 20:55:31 +0900

    210809 leetcode

--- 00. Daily Algorithm/Leetcode/14장.트리 54 - 전위, 중위 순회 결과로 이진트리 구축.py
+++ 00. Daily Algorithm/Leetcode/14장.트리 54 - 전위, 중위 순회 결과로 이진트리 구축.py
@@ -1,2 +1,15 @@
-# https://leetcode.com/problems/
-# 풀이 : 
+# https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+# 풀이 : 전위 순회 결과로 중위 순회 분할 정복
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if inorder:
+            # 전위 순회 결과로 중위 순회 분할 인덱스
+            idx = inorder.index(preorder.pop(0))
+            
+            # 중위 순회 결과 분할 정복
+            node = TreeNode(inorder[idx])
+            node.left = self.buildTree(preorder, inorder[0:idx])
+            node.right = self.buildTree(preorder, inorder[idx+1:])
+            
+            return node
--- 00. Daily Algorithm/Leetcode/15장.힙 55 - 배열의 K번째 큰 요소.py
+++ 00. Daily Algorithm/Leetcode/15장.힙 55 - 배열의 K번째 큰 요소.py
@@ -1,2 +1,19 @@
-# https://leetcode.com/problems/
-# 풀이 : 
+# https://leetcode.com/problems/kth-largest-element-in-an-array
+# 풀이 1 : 최대 heap 만들어보자.
+
+import heapq
+
+def findKthLargest(self, nums: list[int], k: int) -> int:
+    heap = list()
+    for n in nums:
+        heapq.heappush(heap, -n)
+        
+    for _ in range(1, k):
+        heapq.heappop(heap)
+        
+    return -heapq.heappop(heap)
+
+'''
+풀이 2 : heapq 모듈의 heapify 이용
+풀이 3 : heapq 모듈의 nlargest 이용
+'''
