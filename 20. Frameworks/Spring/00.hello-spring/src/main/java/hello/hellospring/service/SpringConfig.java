5e408d65fee93ac4c57899d082b56b3fb45e780a
k-min9 <mingu4969@gmail.com>
2021-08-22 23:55:39 +0900

    210822 spring

--- 20. Frameworks/Spring/00.hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
+++ 20. Frameworks/Spring/00.hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring.service;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -20,28 +21,47 @@ public class SpringConfig {
 //    }
 
     //jpa를 위한 구현
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
-    private EntityManager em;
+//    @Bean
+//    public MemberService memberService(){
+//        return new MemberService(memberRepository());
+//    }
+
+//    @Bean
+//    public MemberRepository memberRepository(){
+    //1.기존의 자바 메모리
+    //return new MemoryMemberRepository();
+    //2.기존의 DB를 갈아끼기만 해도 이게 작동한다. SOLID의 OCP:개방-폐쇄 원칙을 스프링의 DI를 통해 훌륭히 구현
+    //return new JdbcMemberRepository(dataSource);
+    //3.jdbc Template로 구현 (테스트 환경은 동일)
+    //return new JdbcTemplateMemberRepository(dataSource);
+    //4. JPA로 구현
+    //return new JpaMemberRepository(em);
+    //}
+
+    //스프링 데이터 jpa를 위한 구현
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
+    //5. 스프링 데이터 JPA로 구현
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
-        //1.기존의 자바 메모리
-        //return new MemoryMemberRepository();
-        //2.기존의 DB를 갈아끼기만 해도 이게 작동한다. SOLID의 OCP:개방-폐쇄 원칙을 스프링의 DI를 통해 훌륭히 구현
-        //return new JdbcMemberRepository(dataSource);
-        //3.jdbc Template로 구현 (테스트 환경은 동일)
-        //return new JdbcTemplateMemberRepository(dataSource);
-        //4. JPA로 구현
-        return new JpaMemberRepository(em);
-    }
+    //AOP 사용을 명시하기에는 컴퍼넌트 스캔 안쓰는 쪽이 좋다.
+//    @Bean
+//    public TimeTraceAop timeTraceAop(){
+//        return new TimeTraceAop();
+//    }
 }
