ca227549f22c2126c53ca3cfc8ec30ada55a22aa
k-min9 <mingu4969@gmail.com>
2021-09-05 17:21:19 +0900

    210905 spring

---
+++ 20. Frameworks/Spring/06.JPA0-shopOrder/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,52 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @Column(name = "MEMBER_ID")
+    private Long MemberId;
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING) // 안하면 나중에 큰 장애 가능성 있음
+    private OrderStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return MemberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        MemberId = memberId;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
