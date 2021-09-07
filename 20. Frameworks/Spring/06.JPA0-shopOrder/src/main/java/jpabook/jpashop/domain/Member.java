df59d32ec63517bdb3bf930f23911add94856966
k-min9 <mingu4969@gmail.com>
2021-09-08 01:16:06 +0900

    210908 spring mapping

--- 20. Frameworks/Spring/06.JPA0-shopOrder/src/main/java/jpabook/jpashop/domain/Member.java
+++ 20. Frameworks/Spring/06.JPA0-shopOrder/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,9 +1,8 @@
 package jpabook.jpashop.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member {
@@ -17,7 +16,11 @@ public class Member {
    private String street;
    private String zipcode;
 
-    public Long getId() {
+   //역방향 연결 Member-Order(주인)
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
+   public Long getId() {
         return id;
     }
 
--- 20. Frameworks/Spring/06.JPA0-shopOrder/src/main/java/jpabook/jpashop/domain/Order.java
+++ 20. Frameworks/Spring/06.JPA0-shopOrder/src/main/java/jpabook/jpashop/domain/Order.java
@@ -2,6 +2,8 @@ package jpabook.jpashop.domain;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "ORDERS")
@@ -11,12 +13,28 @@ public class Order {
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @Column(name = "MEMBER_ID")
-    private Long MemberId;
+//    @Column(name = "MEMBER_ID")
+//    private Long MemberId;
+
+    //MEMBER과 ORDERS의 단방향 연결
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID") //외래키 기준
+    private Member member;
+
+    //역방향 연결 => 주인 OrderItem의 order
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING) // 안하면 나중에 큰 장애 가능성 있음
     private OrderStatus status;
+
+    // 양방향 편의 메소드 구현
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
 
     public Long getId() {
         return id;
@@ -26,12 +44,12 @@ public class Order {
         this.id = id;
     }
 
-    public Long getMemberId() {
-        return MemberId;
+    public Member getMember() {
+        return member;
     }
 
-    public void setMemberId(Long memberId) {
-        MemberId = memberId;
+    public void setMember(Member member) {
+        this.member = member;
     }
 
     public LocalDateTime getOrderDate() {
@@ -49,4 +67,6 @@ public class Order {
     public void setStatus(OrderStatus status) {
         this.status = status;
     }
+
+
 }
