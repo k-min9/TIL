9032bb68b110df51db392c073ee1023fb50fa64c
k-min9 <mingu4969@gmail.com>
2021-09-23 00:31:17 +0900

    210923 spring login cookie

--- 20. Frameworks/Spring/12.MVC2-login/src/main/java/hello/login/web/HomeController.java
+++ 20. Frameworks/Spring/12.MVC2-login/src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,38 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final MemberRepository memberRepository;
+
+    //로그인 안해도 들어오게 required false
     @GetMapping("/")
-    public String home() {
-        return "home";
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model){
+
+        //로그인 안한 사용자
+        if (memberId == null){
+            return "home";
+        }
+
+        //로그인
+        Member loginMember = memberRepository.findById(memberId);
+        if (memberId == null){
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+
     }
 }
--- 20. Frameworks/Spring/12.MVC2-login/src/main/java/hello/login/web/login/LoginController.java
+++ 20. Frameworks/Spring/12.MVC2-login/src/main/java/hello/login/web/login/LoginController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
@@ -24,7 +27,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult){
+    public String login(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response){
         if (bindingResult.hasErrors()){
             return "login/loginForm";
         }
@@ -39,7 +42,25 @@ public class LoginController {
 
         //로그인 성공 TODO
 
+        //쿠키에 시간 정보를 주지 않으면 세션쿠키
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
+
         return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response){
+        //쿠키의 남은 시간을 0으로 만들면 삭제된다.
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    //extract Method로 뽑음
+    private void expireCookie(HttpServletResponse response, String CookieName) {
+        Cookie cookie = new Cookie(CookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 
 }
