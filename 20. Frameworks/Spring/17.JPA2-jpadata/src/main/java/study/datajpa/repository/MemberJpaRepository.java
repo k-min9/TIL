22de0ae5caa10931c18675ac97559afc73cb15fd
k-min9 <mingu4969@gmail.com>
2021-10-26 20:22:50 +0900

    211026 spring data jpaRepository

--- 20. Frameworks/Spring/17.JPA2-jpadata/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ 20. Frameworks/Spring/17.JPA2-jpadata/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,8 @@ import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemberJpaRepository {
@@ -17,6 +19,27 @@ public class MemberJpaRepository {
         em.persist(member);
         return member;
     }
+
+    public void delete(Member member){
+        em.remove(member);
+    }
+
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    //있을지도 없을지도 모르면 optional!
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count(){
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
+    }
+
 
     public Member find(Long id) {
         return em.find(Member.class, id);
--- 20. Frameworks/Spring/17.JPA2-jpadata/src/main/java/study/datajpa/repository/MemberRepository.java
+++ 20. Frameworks/Spring/17.JPA2-jpadata/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Member;
 
+//@Repository 스프링데이터JPA Repo는 Repository 생략이 가능하다.
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
 }
