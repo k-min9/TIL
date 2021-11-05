0b5353d4e8398917315b055b06365d16ea311f08
k-min9 <mingu4969@gmail.com>
2021-11-06 00:53:41 +0900

    211106 spring querydsl setting and entity

---
+++ 20. Frameworks/Spring/18.Querydsl-querydsl/src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,29 @@
+package study.querydsl.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  // 기본생성자 protected team 만들어서 이것저것 편하게
+@ToString(of = {"id", "name"})
+public class Team {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
