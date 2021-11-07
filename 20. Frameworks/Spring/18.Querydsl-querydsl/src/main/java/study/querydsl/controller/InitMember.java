f87a9c28648ddc9cb66512b4195fba09a3dccbab
k-min9 <mingu4969@gmail.com>
2021-11-07 20:44:20 +0900

    211107 application setting and test case added

---
+++ 20. Frameworks/Spring/18.Querydsl-querydsl/src/main/java/study/querydsl/controller/InitMember.java
@@ -0,0 +1,46 @@
+package study.querydsl.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+
+// applocation.yml에 profile 이름 local로 등록해둠
+// 스프링부트로 기동시 The following profiles are active: local 확인 가능
+@Profile("local")
+@Component
+@RequiredArgsConstructor
+public class InitMember {
+
+    private final InitMemberService initMemberService;
+
+    @PostConstruct
+    public void init() {
+        initMemberService.init();
+    }
+
+    @Component
+    static class InitMemberService {
+        @Autowired private EntityManager em;
+
+        // 위에 init에 바로 넣고 싶지만 Transactional과 PostConstruct 같이 쓰면 동작을 안해서 분리해야된다...
+        @Transactional
+        public void init() {
+            Team teamA = new Team("teamA");
+            Team teamB = new Team("teamB");
+            em.persist(teamA);
+            em.persist(teamB);
+            for (int i = 0; i < 100; i++) {
+                Team selectedTeam = i % 2 == 0 ? teamA : teamB;
+                em.persist(new Member("member" + i, i, selectedTeam));
+            }
+        }
+    }
+
+}
