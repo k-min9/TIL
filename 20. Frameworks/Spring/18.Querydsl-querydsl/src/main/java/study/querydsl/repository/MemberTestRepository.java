b235b369746af8b2c12e8c9c1096d5cf30fc064b
k-min9 <mingu4969@gmail.com>
2021-11-09 19:20:15 +0900

    211108 spring querydsl final advanced custom support class

---
+++ 20. Frameworks/Spring/18.Querydsl-querydsl/src/main/java/study/querydsl/repository/MemberTestRepository.java
@@ -0,0 +1,101 @@
+package study.querydsl.repository;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.stereotype.Repository;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.entity.Member;
+import study.querydsl.repository.support.Querydsl4RepositorySupport;
+import java.util.List;
+
+import static org.springframework.util.StringUtils.hasText;
+import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
+
+/**
+ * Advanced : Querydsl 지원 클래스를 직접 만들어보자
+ * 편안한 Paging 변환 기능, 페이징 카운트 쿼리 분리 기능, 스프링 데이터 Sort 지원하면서 단점을 없애고 기존의 문법대로 작성할 수 있다.
+ * */
+@Repository
+public class MemberTestRepository extends Querydsl4RepositorySupport {
+    public MemberTestRepository() {
+        super(Member.class);
+    }
+
+    public List<Member> basicSelect() {
+        return select(member)
+                .from(member)
+                .fetch();
+    }
+
+    public List<Member> basicSelectFrom() {
+        return selectFrom(member)
+                .fetch();
+    }
+
+    // 페이지와 소트까지 지원되게 다 짜둠
+    public Page<Member> searchPageByApplyPage(MemberSearchCondition condition, Pageable pageable) 
+    {
+        JPAQuery<Member> query = 
+                selectFrom(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()));
+        
+        List<Member> content = getQuerydsl().applyPagination(pageable, query)
+                .fetch();
+        return PageableExecutionUtils.getPage(content, pageable, query::fetchCount);
+    }
+
+    public Page<Member> applyPagination(MemberSearchCondition condition, Pageable pageable) {
+
+        // 첫번째는 페이지, 두번째 쿼리(function을 람다로 깨끗하게)를 넘김
+        return applyPagination(pageable, contentQuery -> contentQuery
+                .selectFrom(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())));
+    }
+
+    // 카운트 쿼리 분리버전
+    public Page<Member> applyPagination2(MemberSearchCondition condition, Pageable pageable) {
+        
+        return applyPagination(pageable, contentQuery -> contentQuery
+                        .selectFrom(member)
+                        .leftJoin(member.team, team)
+                        .where(usernameEq(condition.getUsername()),
+                                teamNameEq(condition.getTeamName()),
+                                ageGoe(condition.getAgeGoe()),
+                                ageLoe(condition.getAgeLoe())
+                        ), countQuery -> countQuery //여기서부터 카운트 쿼리
+                        .selectFrom(member)
+                        .leftJoin(member.team, team)
+                        .where(usernameEq(condition.getUsername()),
+                                teamNameEq(condition.getTeamName()),
+                                ageGoe(condition.getAgeGoe()),
+                                ageLoe(condition.getAgeLoe()))
+        );
+    }
+
+    private BooleanExpression usernameEq(String username) {
+        return hasText(username) ? member.username.eq(username) : null;
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return hasText(teamName) ? team.name.eq(teamName) : null;
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe == null ? null : member.age.goe(ageGoe);
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe == null ? null : member.age.loe(ageLoe);
+    }
+}
