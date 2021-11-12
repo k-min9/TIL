7c16760b22ea303977b4a0147762f9ae21ce1271
k-min9 <mingu4969@gmail.com>
2021-11-13 00:50:27 +0900

    211113 BigDataAnalyst

--- 80. Certificate/빅데이터분석기사/06_ML프로세스_예시2_회귀문제.py
+++ 80. Certificate/빅데이터분석기사/06_ML프로세스_예시2_회귀문제.py
@@ -8,19 +8,19 @@
 '''
 # 1. 자료 읽기
 import pandas as pd
-data=pd.read_csv('05_ML프로세스_예시1_분류문제_cancer.csv', encoding='utf-8')
+data=pd.read_csv('06_ML프로세스_예시2_회귀문제_house_price.csv', encoding='utf-8')
 
 # 자료 설명 : 목적 변수 : 'class', 유방암 여부 이진(0/1) 데이터
 # print(data)
 
 # 2. 특성(x)과 레이블(y) 나누기
 # X = data[['이름1','이름2',...]]  # 방법 1
-X = data[data.columns[1:10]]  # 방법 2 : 연달아있으면 오타도 없고 이게 편함, 떨어져 있으면 # X = data[data.columns[[0,2,4]]]
-Y = data['Class']
+X = data[data.columns[0:5]]  # 방법 2 : 연달아있으면 오타도 없고 이게 편함, 떨어져 있으면 # X = data[data.columns[[0,2,4]]]
+Y = data[["house_value"]]
 
 # 3. Data Set 분할
 from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, stratify=Y, random_state=42)  # stratify : 범주 비율 맞추기, random_state : 시드 값
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=42)  # stratify : 범주 비율 맞추기(회귀는 y가 범주가 아니니까 사용불가!), random_state : 시드 값
 
 # 4. 정규화 : 특성치 단위 맞추기 (민맥스, 표준화 둘 다 예시)
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
@@ -34,27 +34,23 @@ scaler_STD.fit(X_train)
 X_train_STD = scaler_STD.transform(X_train)
 X_test_STD = scaler_STD.transform(X_test)
 
-# 5. 모델 학습 (예시 - 로지스틱 회귀 모델)
-from sklearn.linear_model import LogisticRegression
-model = LogisticRegression()
+# 5. 모델 학습 (예시 - 선형 회귀 모델)
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
 model.fit(X_train_MINMAX, Y_train)
 pred_train = model.predict(X_train_MINMAX)  # predict : 예측결과
-pred_train_proba = model.predict_proba(X_train_MINMAX)  # predict_proba : 범주 1이 나올 확률
 pred_test = model.predict(X_test_MINMAX)
 score = model.score(X_train_MINMAX, Y_train)  # score : 정확도 
 # print(score)
 
-# 기타 혼돈 행렬(confusion matrix), 분류 예측 레포트(classification report), ROC 지표 등을 이용한 정확도 판별 스킵
+# MSE 등을 사용하여 틀린정도를 확인 가능
+# from sklearn.metrics import mean_squared_error
+# MSE = mean_squared_error(Y_test, pred_test)
+# print(MSE**0.5)
 
-# Y_test[['y_pred']] = pred_test
-'''
+
 # 6. 예측값 병합 및 저장 [작동체크]
-# print(Y_train)
-# Y_train[['y_pred']] = pred_train  # 열 추가하며 저장
-# Y_train['y_prob0', 'y_prob1'] = pred_train_proba
 Y_test[['y_pred']] = pred_test
-Y_test[['y_prob0', 'y_prob1']] = pred_train_proba
-'''
 
 answers = pd.concat([X_test, Y_test], axis=1)  # 열로 조합
 answers.to_csv('new_data.csv')
