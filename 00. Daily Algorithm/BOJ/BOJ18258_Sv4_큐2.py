ce898971a0e9beaa30643e4ea3f51d7521b4f6ac
k-min9 <mingu4969@gmail.com>
2021-12-20 18:27:20 +0900

    211220 BOJ

---
+++ 00. Daily Algorithm/BOJ/BOJ18258_Sv4_큐2.py
@@ -0,0 +1,36 @@
+'''
+직접 자료구조를 활용하여 구현해보는 문제
+'''
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+q = deque([])
+
+for i in range(n):
+    op = input().split()
+    if op[0] == "push":
+        q.appendleft(int(op[1]))
+    elif op[0] == "front":
+        if q:
+            print(q[-1])
+        else:
+            print(-1)
+    elif op[0] == "back":
+        if q:
+            print(q[0])
+        else:
+            print(-1)
+    elif op[0] == "size":
+        print(len(q))
+    elif op[0] == "empty":
+        if not q:
+            print(1)
+        else:
+            print(0)
+    else:
+        if not q:
+            print(-1)
+        else:
+            print(q.pop())
