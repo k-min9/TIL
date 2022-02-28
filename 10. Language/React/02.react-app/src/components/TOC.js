e1464ae202df90bc7489ac8b1eb0303df85f5b11
k-min9 <mingu4969@gmail.com>
2022-02-28 18:55:59 +0900

    220228 React init component

---
+++ 10. Language/React/02.react-app/src/components/TOC.js
@@ -0,0 +1,17 @@
+import {Component} from 'react';
+
+class TOC extends Component{
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>1. 1번요소</li>
+          <li>2. 2번요소</li>
+          <li>3. 3번요소</li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default TOC;
