e0989406798d46277009a91e0405ca78f3a3360b
k-min9 <mingu4969@gmail.com>
2022-03-01 14:04:33 +0900

    220301 react event state component

--- 10. Language/React/02.react-app/src/components/TOC.js
+++ 10. Language/React/02.react-app/src/components/TOC.js
@@ -23,7 +23,19 @@ class TOC extends Component{
     var i = 0;
     while (i < data.length) {
       // 내부적 unique 값인 key 부여 잊지 말 것
-      lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+      lists.push(
+        <li key={data[i].id}>
+          <a // 이러한 태그를 function(e)에서는 e.target으로 정의 함
+            href={"/content/"+data[i].id}
+            // 접미사(-)가 붙으면 e.dataset이라는 특수한 식으로 접근 가능
+            // e.target.dataset.id
+            data-id={data[i].id}  
+            onClick={function(e){
+              e.preventDefault();
+              this.props.onChangePage(e.target.dataset.id);
+            }.bind(this)}>{data[i].title}
+          </a>
+        </li>);
       i = i + 1;
     }
     return (
