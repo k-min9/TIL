6580c0e909388b95edc6a3a7b50afc9298ee6661
k-min9 <mingu4969@gmail.com>
2022-03-03 00:23:50 +0900

    220303 react useState

--- 10. Language/React/03.react-app-function/src/App.js
+++ 10. Language/React/03.react-app-function/src/App.js
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import Button from "./Button";
 import styles from "./App.module.css";
 
 function App() {
+  const [counter, setValue] = useState(0);  // value, modifier, default = 0
+  const onClick = () => setValue((prev) => prev + 1);
+
   return (
     <div>
       <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"}/>
+      <h2>{counter}</h2>
+      <Button text={"props!"}/>
+      <button onClick={onClick}>카운터 증가</button>
     </div>
   );
 }
