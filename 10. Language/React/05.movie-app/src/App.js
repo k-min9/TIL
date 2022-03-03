9c2af1eb65e9cd94e54978b3a2cd8248c82bb764
k-min9 <mingu4969@gmail.com>
2022-03-03 16:10:42 +0900

    220303 react movieapp init async await fetch

---
+++ 10. Language/React/05.movie-app/src/App.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);  // 로딩 메시지 종료~
+  };
+  useEffect(() => {
+    getMovies();  // 단순 분리 이상으로 async ~await을 쓰겠다는 의도가 있다.
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} alt="poster"/>
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+export default App;
