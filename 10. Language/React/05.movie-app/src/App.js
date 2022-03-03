8ffa471e35b11c08a8b5aeb7eb3ce4f3f5c4c9c7
k-min9 <mingu4969@gmail.com>
2022-03-03 17:14:15 +0900

    220303 react component before route

--- 10. Language/React/05.movie-app/src/App.js
+++ 10. Language/React/05.movie-app/src/App.js
@@ -1,41 +1,5 @@
-import { useEffect, useState } from "react";
-
+// 이제부터 App은 render을 router 할 것 임
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);  // 로딩 메시지 종료~
-  };
-  useEffect(() => {
-    getMovies();  // 단순 분리 이상으로 async ~await을 쓰겠다는 의도가 있다.
-  }, []);
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} alt="poster"/>
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 export default App;
