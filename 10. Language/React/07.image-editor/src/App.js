e2a1d031e0b3bc89c85affc69da72b007a6d0d32
k-min9 <mingu4969@gmail.com>
2022-03-21 22:06:33 +0900

    220321 react ipfs image upload

--- 10. Language/React/07.image-editor/src/App.js
+++ 10. Language/React/07.image-editor/src/App.js
@@ -10,6 +10,9 @@ function App() {
   const imageSERVER = "https://cdn.filestackcontent.com/AM9o5lgXYR1uvQX2NaAqnz/output=secure:true/"
   const editorRef = useRef(null);
   const FileSaver = require('file-saver')  // 로컬 저장용 (npm install file-saver)
+
+  const ipfsClient = require('ipfs-http-client')
+  const ipfs = ipfsClient.create({ host: 'j6a102.p.ssafy.io', port: '5001', protocol: 'http' })
     
   const editorFlipX = () => {
     // const editorInstance = editorRef.current.getInstance();
@@ -38,10 +41,29 @@ function App() {
       editorInstance.resize({width:200, height:150} 
     )}
   );}
-  const editorToBase64 = () => {
+
+  const editorToBase64 = async () => {
     const editorInstance = editorRef.current.getInstance();
-    var res = editorInstance.toDataURL();
-    console.log(res)
+    var image = editorInstance.toDataURL();
+
+    // image to Blob(Binary Large Object)
+    var byteString = window.atob(image.split(',')[1]);
+    var mimeString = image.split(',')[0].split(':')[1].split(';')[0]
+    var ab = new ArrayBuffer(byteString.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+    var blob = new Blob([ab], {type: mimeString});
+    // console.log('블롭', blob)
+
+    try{
+      const result = await ipfs.add(blob) 
+      console.log("결과", result);
+      console.log("주소", result.path)
+    } catch(e){
+      console.log("에러 : ", e)
+    }
   }
 
   return (
@@ -53,6 +75,7 @@ function App() {
             // 기본사진
             path: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/be0a9a4b-1957-4a83-a542-fb720a0a660e/d17qhno-e7454092-80aa-4b3d-898a-96198b4a756c.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2JlMGE5YTRiLTE5NTctNGE4My1hNTQyLWZiNzIwYTBhNjYwZVwvZDE3cWhuby1lNzQ1NDA5Mi04MGFhLTRiM2QtODk4YS05NjE5OGI0YTc1NmMuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.Kntd9m3HPNC-JF5QcIS3Iew_dNN68BOt4oeiOTj_Rgs",
             // path: "https://docs.unity3d.com/uploads/Main/ShadowIntro.png",
+            // path: "https://ipfs.io/ipfs/QmYGNuov5cNibDyzL2kqvhfrgxxkaDnKaggokWPWgtN7dQ",
             name: "SampleImage",
           },
           theme: myTheme,
