ef3ea9937862aabde7a5f998a09fb2fb01fd8dc8
k-min9 <mingu4969@gmail.com>
2022-04-02 12:57:13 +0900

    220402 BOJ java init&setting

--- 00. Daily Algorithm/BOJ/BOJ9012_Sv4_괄호.java
+++ 00. Daily Algorithm/BOJ/BOJ9012_Sv4_괄호.java
@@ -11,7 +11,6 @@ public class BOJ9012_Sv4_괄호 {
 	public static void main(String[] args) {
  
 		Scanner in = new Scanner(System.in);
-		
 		int T = in.nextInt();
 		
 		for(int i = 0; i < T; i++) {
@@ -31,9 +30,7 @@ public class BOJ9012_Sv4_괄호 {
 			if (c == '(') {
 				stack.push(c);
 			}
- 
-			// 아래는 모두 닫는 괄호 일 경우들이다.
- 
+ 			// 아래는 모두 닫는 괄호 일 경우들이다.
 			// 스택이 비어있는 경우. 즉, 닫는 괄호를 입력받았으나 pop할 원소가 없을 경우
 			else if (stack.empty()) {
 				return "NO";
@@ -48,7 +45,7 @@ public class BOJ9012_Sv4_괄호 {
 		 * 모든 검사 마치고 스택에 잔여 요소가 있으면 여는 괄호가 많은 경우는 "NO" 
 		 * 스택이 비어있으면 온전한 수식이므로 "YES" 이다.
 		 */
- 
+
 		if (stack.empty()) {
 			return "YES";
 		} 
