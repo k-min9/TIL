d3c0abf45f8319392dca89420371d3335e702f36
k-min9 <mingu4969@gmail.com>
2022-04-12 20:53:10 +0900

    220412 spring cloud gateway filters

---
+++ 20. Frameworks/Spring/22.Cloud-Gateway/gateway-service/src/main/java/com/example/gatewayservice/FilterConfig.java
@@ -0,0 +1,25 @@
+package com.example.gatewayservice;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class FilterConfig {
+
+//    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/first-service/**")
+                        // request나 response에 헤더를 붙여서 받거나 내보낼 수 있음
+                        .filters(f -> f.addRequestHeader("first-request", "first-request-header")
+                                       .addResponseHeader("first-response", "first-response-header"))
+                        .uri("http://localhost:8081"))
+                .route(r -> r.path("/second-service/**")
+                        .filters(f -> f.addRequestHeader("second-request", "second-request-header")
+                                .addResponseHeader("second-response", "second-response-header"))
+                        .uri("http://localhost:8082"))
+                .build();
+    }
+}
