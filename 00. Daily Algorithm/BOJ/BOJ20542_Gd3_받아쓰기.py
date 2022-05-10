98b2529d0e9e71806e2db037e8113b0b993453d0
k-min9 <mingu4969@gmail.com>
2022-05-10 09:37:43 +0900

    220510 BOJ

---
+++ 00. Daily Algorithm/BOJ/BOJ20542_Gd3_받아쓰기.py
@@ -0,0 +1,22 @@
+'''
+추가 삭제 변환
+2차원 DP로 편집 내용 저장
+'''
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+w1 = input()
+w2 = input()
+
+dp = [[i]+[0]*m for i in range(n+1)]
+dp[0] = [i for i in range(m+1)]
+
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])+1
+        # 편집 여부 체크
+        if w1[i-1]==w2[j-1] or (w1[i-1]=='v' and w2[j-1]=='w') or (w1[i-1]=='i' and w2[j-1] in ['j', 'l']):
+            dp[i][j] = dp[i-1][j-1]
+
+print(dp[n][m])
