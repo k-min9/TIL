b53c2d8a057b9bfb6493a8ca1b54e19083004cae
k-min9 <mingu4969@gmail.com>
2022-05-30 06:41:56 +0900

    220530 BOJ

---
+++ 00. Daily Algorithm/BOJ/BOJ17626_Sv4_FourSquares.py
@@ -0,0 +1,20 @@
+'''
+상식도 쌓고 DP긴 한데 이거 흠...
+'''
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+dp = [0,1]
+
+for i in range(2, N+1):
+    min_value = 1e9
+    j = 1
+
+    while (j**2) <= i:
+        min_value = min(min_value, dp[i - (j**2)])
+        j += 1
+
+    dp.append(min_value + 1)
+
+print(dp[N])
