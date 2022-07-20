e9aac2e14a38215503fc81897534b96ccf542b93
k-min9 <mingu4969@gmail.com>
2022-07-20 14:06:24 +0900

    220720 BOJ

---
+++ 00. Daily Algorithm/BOJ/BOJ1926_Sv1_그림.py
@@ -0,0 +1,45 @@
+'''
+전형적인 BFS 문제 0분침하는거 보여드림
+'''
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+
+MOVES = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append([x, y])
+    graphs[x][y] = 0
+    size = 1
+
+    while queue:
+        x, y = queue.popleft()
+        for dx, dy in MOVES:
+            nx = x + dx
+            ny = y + dy
+
+            if 0 <= nx < N and 0 <= ny < M and graphs[nx][ny] == 1:
+                size += 1
+                graphs[nx][ny] = 0
+                queue.append([nx, ny])
+
+    answers.append(size)
+
+
+N, M = map(int, input().split())
+graphs = [list(map(int, input().split())) for _ in range(N)]
+
+answers = list()
+for x in range(N):
+    for y in range(M):
+        if graphs[x][y] == 1:
+            bfs(x, y)
+
+print(len(answers))
+print(max(answers))
+
+'''
+ㅎㅎㅎ!
+'''
