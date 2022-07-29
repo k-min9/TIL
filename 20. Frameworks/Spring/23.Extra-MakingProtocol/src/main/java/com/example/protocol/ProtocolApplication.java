a057f02c6e90b354fc0840eb6c03cbfdda99b27e
k-min9 <mingu4969@gmail.com>
2022-07-29 13:25:21 +0900

    220729 spring protocol init

---
+++ 20. Frameworks/Spring/23.Extra-MakingProtocol/src/main/java/com/example/protocol/ProtocolApplication.java
@@ -0,0 +1,13 @@
+package com.example.protocol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtocolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtocolApplication.class, args);
+	}
+
+}
---
+++ 20. Frameworks/Spring/23.Extra-MakingProtocol/src/test/java/com/example/protocol/ProtocolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.protocol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtocolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
