8e8685e463967fb1d800f80f7a8aecbdbff93a61
k-min9 <mingu4969@gmail.com>
2022-09-02 00:48:18 +0900

    220902 spring adv 로그추적기 연결 V2 fin

---
+++ 20. Frameworks/Spring/30.Advanced-로그 추적기/src/main/java/advanced/hello/app/v2/OrderControllerV2.java
@@ -0,0 +1,30 @@
+package advanced.hello.app.v2;
+
+import advanced.hello.trace.TraceStatus;
+import advanced.hello.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+    private final HelloTraceV2 trace;
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(status.getTraceId(), itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;//예외를 꼭 다시 던져주어야 한다.
+        }
+    }
+}
