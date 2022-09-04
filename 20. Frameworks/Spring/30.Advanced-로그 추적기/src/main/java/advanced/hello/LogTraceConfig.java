46aecab7fe3b893b48d333ce615a65cce3e279ba
k-min9 <mingu4969@gmail.com>
2022-09-05 00:40:32 +0900

    220905 spring adv ThreadLocal

---
+++ 20. Frameworks/Spring/30.Advanced-로그 추적기/src/main/java/advanced/hello/LogTraceConfig.java
@@ -0,0 +1,15 @@
+package advanced.hello;
+
+import advanced.hello.trace.logtrace.LogTrace;
+import advanced.hello.trace.logtrace.ThreadLocalLogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogTraceConfig {
+
+    @Bean
+    public LogTrace logTrace() {
+        return new ThreadLocalLogTrace();
+    }
+}
