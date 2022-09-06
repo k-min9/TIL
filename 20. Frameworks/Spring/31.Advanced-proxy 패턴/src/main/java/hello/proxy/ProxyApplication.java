09eddc6336c79d3d7b9e3c104c4768f3d0bc2961
k-min9 <mingu4969@gmail.com>
2022-09-06 09:45:01 +0900

    220906 spring adv proxy V2

--- 20. Frameworks/Spring/31.Advanced-proxy 패턴/src/main/java/hello/proxy/ProxyApplication.java
+++ 20. Frameworks/Spring/31.Advanced-proxy 패턴/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,11 +1,13 @@
 package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class)  // Config를 Import로 수동 등록하는거 잊지 말자
+//@Import(AppV1Config.class)  // Config를 Import로 수동 등록하는거 잊지 말자
+@Import({AppV1Config.class, AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") // 컴포넌트 스캔 대상 app 범위 내로 축소(다른 실습도 있으니까) 
 public class ProxyApplication {
 
