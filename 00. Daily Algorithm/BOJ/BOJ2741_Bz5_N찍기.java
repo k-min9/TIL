c83c7739022b3dc1c54f55aa91a83830ecd1be50
k-min9 <mingu4969@gmail.com>
2022-09-08 09:12:09 +0900

    220908 BOJ

---
+++ 00. Daily Algorithm/BOJ/BOJ2741_Bz5_N찍기.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * BOJ2741_Bz5_N찍기
+ */
+public class BOJ2741_Bz5_N찍기 {
+
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+
+    for (int i = 1; i <= N; i++) {
+      System.out.println(i);
+    }
+
+    sc.close();
+  }
+}
