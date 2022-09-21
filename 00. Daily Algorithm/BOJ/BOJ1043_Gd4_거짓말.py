9a11101fe1ce281afb0e736873323f8b0ea32cdc
k-min9 <mingu4969@gmail.com>
2022-09-21 09:27:00 +0900

    220921 BOJ

---
+++ 00. Daily Algorithm/BOJ/BOJ1043_Gd4_거짓말.py
@@ -0,0 +1,25 @@
+'''
+set 찾는게 O(1)을 아는지 물어보는 문제
+'''
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+knowList = set(input().split()[1:])
+parties = []
+
+for _ in range(m):
+    parties.append(set(input().split()[1:]))
+
+for _ in range(m):
+    for party in parties:
+        if party & knowList:
+            knowList = knowList.union(party)
+
+cnt = 0
+for party in parties:
+    if party & knowList: # 크
+        continue
+    cnt += 1
+
+print(cnt)
