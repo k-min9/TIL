681236880cf571a1e87ecdfb0d3d7d8fdb129d51
k-min9 <mingu4969@gmail.com>
2022-11-12 19:41:39 +0900

    221112 dart init

---
+++ 10. Language/Dart/01.변수.dart
@@ -0,0 +1,22 @@
+void main() {
+  // 변수 선언
+  var name = 'k-min9';
+  var number = 10;
+  print(name);
+  
+  // 변수 타입
+  int num1 = 10;
+  int num2 = 20;
+  print(num1 + num2);
+  
+  double d1 = 2.5;
+  bool isTrue = true;
+  String name2 = 'k-min9';
+  
+  print('${name} ${name2} : ${num1}');
+  
+  // dynamic
+  dynamic dynamic1 = 'a';
+  dynamic1 = 2;  // var과 다르게 중간에 타입 자체를 바꿔버릴 수 있음
+  
+}
