3deb6c61bd1685b3e4e51baf668f3a65c77af00c
k-min9 <mingu4969@gmail.com>
2023-01-26 00:48:10 +0900

    230126 flutter project api 추가

--- 90. Projects/03. GeoAlarm/lib/location_service.dart
+++ 90. Projects/03. GeoAlarm/lib/location_service.dart
@@ -2,13 +2,25 @@ import 'dart:convert';
 import 'package:geo_alarm/app_constant.dart';
 import 'package:http/http.dart' as http;
 
-Future<http.Response> getLocationData(String text) async {
+Future<http.Response> getSearchData(String text) async {
   http.Response response;
   
   response = await http.get(
     // Uri.parse("http://mvs.bslmeiyu.com/api/v1/config/place-api-autocomplete?search_text=$text"),
     Uri.parse("https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${text}&key="+AppConstants.APIKEY2),
     headers: {"Context-Type": "application/json"}
+  );
+
+  return response;
+}
+
+Future<http.Response> getLocDataFromPlaceId(String placeId) async {
+  http.Response response;
+
+  response = await http.get(
+    // Uri.parse("http://mvs.bslmeiyu.com/api/v1/config/place-api-autocomplete?search_text=$text"),
+      Uri.parse("https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key="+AppConstants.APIKEY2),
+      headers: {"Context-Type": "application/json"}
   );
 
   return response;
