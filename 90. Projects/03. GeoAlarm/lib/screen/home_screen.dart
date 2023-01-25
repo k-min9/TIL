b569a47debbb8b7a8f1ee557aef7c9dbdd8b9deb
k-min9 <mingu4969@gmail.com>
2023-01-26 00:48:53 +0900

    230126 flutter project home screen 구조 변경 및 기능 추가

--- 90. Projects/03. GeoAlarm/lib/screen/home_screen.dart
+++ 90. Projects/03. GeoAlarm/lib/screen/home_screen.dart
@@ -16,142 +16,219 @@ class _HomeScreenState extends State<HomeScreen> {
   // 구글 맵 컨트롤러 최상단에 생성하고 이벤트 제어
   GoogleMapController? mapController;
 
+  static bool isAlarmOn = false;
+
   // 카메라 중간위치 : latitude - 위도 , longitude - 경도
   static LatLng targetLatLng = LatLng(
     37.5233273,
     126.921252,
   );
-
-  static final CameraPosition initialPosition = CameraPosition(
+  static double _zoom = 15;
+  static CameraPosition _cameraPosition = CameraPosition(
     target: targetLatLng,
-    zoom: 15, // 카메라 확대 정도
+    zoom: _zoom, // 카메라 확대 정도
   );
 
   // 거리 내 원 만들기
-  static final double distRadius = 100;
-  static final Circle circleWithIn = Circle(
-      circleId: CircleId('circleWithIn'),  // circle간 식별자
+  static double distRadius = 100;
+  static Circle circleWithIn = Circle(
+      circleId: CircleId('circleWithIn'), // circle간 식별자
       center: targetLatLng,
       fillColor: Colors.red.withOpacity(0.5), // 원 색
       strokeColor: Colors.red, // 테두리 색
       strokeWidth: 1, // 테두리 두께
-      radius: distRadius
-  );
-  static final Circle circleNotWithIn = Circle(
-      circleId: CircleId('circleNotWithIn'),  // circle간 식별자
+      radius: distRadius);
+  static Circle circleNotWithIn = Circle(
+      circleId: CircleId('circleNotWithIn'), // circle간 식별자
       center: targetLatLng,
       fillColor: Colors.blue.withOpacity(0.5), // 원 색
       strokeColor: Colors.blue, // 테두리 색
       strokeWidth: 1, // 테두리 두께
-      radius: distRadius
-  );
+      radius: distRadius);
 
   // 타겟에 꽂을 마커
   static final Set<Marker> _markers = {};
 
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<LocationController>(builder: (locationController){
+    return GetBuilder<LocationController>(builder: (locationController) {
       return Scaffold(
-        appBar: renderAppBar(),
-        // snapshot의 리턴 형태를 generic
-        body: Stack(
-          children: [
-            FutureBuilder<String>(
-              future: checkPermission(), // 이 함수의 return이 snapshot에 담김
-              // future가 변경될때마다 builder가 재실행
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                // 로딩 중에 원형 로딩 indicator
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-
-                if (snapshot.data == '위치 권한이 허가 되었습니다.') {
-                  return StreamBuilder<Position>(
-                    stream: Geolocator.getPositionStream(), // position 값을 받아 snapshot에 넣음
-                    builder: (context, snapshot) {
-                      // 범위 안에 있나 체크
-                      bool isWithinRange = false;
-
-                      if (snapshot.hasData) {
-                        final start = snapshot.data!;
-                        final end = targetLatLng;
-
-                        final distance = Geolocator.distanceBetween(
-                          start.latitude,
-                          start.longitude,
-                          end.latitude,
-                          end.longitude,
-                        );
-
-                        if (distance < distRadius) {
-                          isWithinRange = true;
-                        }
-                      }
-
-                      return Column(
-                        children: [
-                          _CustomGoogleMap(
-                            initialPosition: initialPosition,
-                            circle: isWithinRange
-                                ? circleWithIn
-                                : circleNotWithIn,
-                            markers: _markers,
-                            onMapCreated: onMapCreated,
-                            targetLatLng: targetLatLng,
-                          ),
-                          _MyButton(),
-                        ],
-                      );
-                    },
-                  );
-                }
-
-                return Center(
-                  child: Text(snapshot.data),
-                );
-              },
+          appBar: AppBar(
+            title: Text(
+              'GeoAlarm',
+              style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 26,
+                  fontWeight: FontWeight.w900),
+              textAlign: TextAlign.center,
             ),
-            Positioned(
-                top: 0,
-                left: 10,
-                right:20,
-                child: GestureDetector(
-                    onTap: (){
-                      Get.dialog(LocationSearchDialog(mapController: mapController));
-                    },
-                    child: Container(
-                        height: 50,
-                        padding: EdgeInsets.symmetric(horizontal: 5),
-                        decoration: BoxDecoration(color: Theme.of(context).cardColor, borderRadius: BorderRadius.circular(10)),
-                        child: Row(children: [
-                          Icon(Icons.location_on, size: 25, color: Theme.of(context).primaryColor),
-                          SizedBox(width: 5),
-                          Expanded(
-                            child: Text(
-                              '${locationController.pickPlaceMark.name ?? ''}'
-                                  ' ${locationController.pickPlaceMark.locality ?? ''} '
+            backgroundColor: Colors.white,
+            actions: [
+              IconButton(
+                onPressed: () async {
+                  // 아직 구글 맵 생성 안 됨
+                  if (mapController == null) {
+                    return;
+                  }
+
+                  final location = await Geolocator.getCurrentPosition();
+
+                  // 맵 컨트롤러로 카메라 위치 이동
+                  mapController!.animateCamera(
+                    CameraUpdate.newLatLng(
+                      LatLng(
+                        location.latitude,
+                        location.longitude,
+                      ),
+                    ),
+                  );
+                },
+                color: Colors.blue,
+                icon: Icon(
+                  Icons.my_location,
+                ),
+              ),
+            ],
+          ),
+          // snapshot의 리턴 형태를 generic
+          body: Stack(
+            children: [
+              FutureBuilder<String>(
+                future: checkPermission(), // 이 함수의 return이 snapshot에 담김
+                // future가 변경될때마다 builder가 재실행
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  // 최초 로딩 중에 원형 로딩 indicator
+                  // if (snapshot.connectionState == ConnectionState.waiting) {
+                  if (!snapshot.hasData) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+
+                  if (snapshot.data == '위치 권한이 허가 되었습니다.') {
+                    return StreamBuilder<Position>(
+                      stream: Geolocator
+                          .getPositionStream(), // position 값을 받아 snapshot에 넣음
+                      builder: (context, snapshot) {
+                        // 범위 안에 있나 체크
+                        bool isWithinRange = false;
+
+                        if (snapshot.hasData) {
+
+                          targetLatLng = locationController.pickLatLng;
+                          final start = snapshot.data!;
+                          final end = targetLatLng;
+                          
+                          // 맵 컨트롤러로 카메라 위치 이동
+                          if (locationController.loading == true){
+                            // 선택된 장소가 있음
+                            mapController!.animateCamera(
+                              CameraUpdate.newLatLng(
+                                LatLng(
+                                  end.latitude,
+                                  end.longitude,
+                                ),
+                              ),
+                            );
+                          } else if (isAlarmOn == false) {
+                            // 고른것도 없으면, 일단 현재 위치로
+                            mapController!.animateCamera(
+                              CameraUpdate.newLatLng(
+                                LatLng(
+                                  start.latitude,
+                                  start.longitude,
+                                ),
+                              ),
+                            );
+                          }
+
+                          final distance = Geolocator.distanceBetween(
+                            start.latitude,
+                            start.longitude,
+                            end.latitude,
+                            end.longitude,
+                          );
+
+                          if (distance < distRadius) {
+                            isWithinRange = true;
+                          }
+                        }
+
+                        return Column(
+                          children: [
+                            Expanded(
+                              flex: 2,
+                              child: GoogleMap(
+                                mapType: MapType.normal,
+                                initialCameraPosition: _cameraPosition,
+                                myLocationEnabled: true,
+                                myLocationButtonEnabled: true,
+                                circles: Set.from([
+                                  isWithinRange ? circleWithIn : circleNotWithIn
+                                ]),
+                                markers: _markers,
+                                onMapCreated: onMapCreated,
+                                onTap: setMarkerOnPos, // 지도 선택시 마커 변경
+                                // targetLatLng: targetLatLng,
+                              ),
+                            ),
+                            // 버튼 입력
+                            Expanded(child: Text('AAA')),
+                          ],
+                        );
+                      },
+                    );
+                  }
+
+                  return Center(
+                    child: Text(snapshot.data),
+                  );
+                },
+              ),
+              Positioned(
+                  top: 0,
+                  left: 10,
+                  right: 20,
+                  child: GestureDetector(
+                      onTap: () {
+                        Get.dialog(
+                            LocationSearchDialog(mapController: mapController));
+                      },
+                      child: Container(
+                          height: 50,
+                          padding: EdgeInsets.symmetric(horizontal: 5),
+                          decoration: BoxDecoration(
+                              color: Theme.of(context).cardColor,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Row(
+                            children: [
+                              Icon(Icons.location_on,
+                                  size: 25,
+                                  color: Theme.of(context).primaryColor),
+                              SizedBox(width: 5),
+                              Expanded(
+                                child: Text(
+                                  '${locationController.pickPlaceMark.name ?? ''} '
+                                  '${locationController.pickPlaceMark.locality ?? ''} '
                                   '${locationController.pickPlaceMark.postalCode ?? ''} '
                                   '${locationController.pickPlaceMark.country ?? ''}',
-                              style: TextStyle(fontSize: 20),
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                          SizedBox(width: 10),
-                          Icon(Icons.search, size: 25, color: Theme.of(context).textTheme.bodyText1!.color)
-                        ],)
-                    )
-                )
-            ),
-          ],
-        )
-
-      );
+                                  style: TextStyle(fontSize: 20),
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                              SizedBox(width: 10),
+                              Icon(Icons.search,
+                                  size: 25,
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .bodyText1!
+                                      .color)
+                            ],
+                          )))),
+            ],
+          ));
     });
-
   }
 
   // 권한 관련은 다 여기서 작업. always일때는 background에서도 동작
@@ -180,107 +257,35 @@ class _HomeScreenState extends State<HomeScreen> {
     return '위치 권한이 허가 되었습니다.';
   }
 
-  // 구글 맵 생성시 GoogleMapController를 state로 저장
-  onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
-
-  AppBar renderAppBar() {
-    return AppBar(
-      title: Text(
-        'GeoAlarm',
-        style: TextStyle(
-            color: Colors.blue, fontSize: 26, fontWeight: FontWeight.w900),
-        textAlign: TextAlign.center,
-      ),
-      backgroundColor: Colors.white,
-      actions: [
-        IconButton(
-          onPressed: () async {
-            // 아직 구글 맵 생성 안 됨
-            if (mapController == null) {
-              return;
-            }
-
-            final location = await Geolocator.getCurrentPosition();
-
-            // 맵 컨트롤러로 카메라 위치 이동
-            mapController!.animateCamera(
-              CameraUpdate.newLatLng(
-                LatLng(
-                  location.latitude,
-                  location.longitude,
-                ),
-              ),
-            );
-          },
-          color: Colors.blue,
-          icon: Icon(
-            Icons.my_location,
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class _MyButton extends StatelessWidget {
-  const _MyButton({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(child: Text('AAA'));
-  }
-}
-
-class _CustomGoogleMap extends StatefulWidget {
-  final CameraPosition initialPosition;
-  final Circle circle;
-  final Set<Marker> markers;
-  final MapCreatedCallback onMapCreated;
-  final LatLng targetLatLng;
-
-  const _CustomGoogleMap({
-    required this.initialPosition,
-    required this.circle,
-    required this.markers,
-    required this.onMapCreated,
-    required this.targetLatLng,
-    Key? key
-  }) : super(key: key);
-
-  @override
-  State<_CustomGoogleMap> createState() => _CustomGoogleMapState();
-}
-
-class _CustomGoogleMapState extends State<_CustomGoogleMap> {
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      flex: 2,
-      child: GoogleMap(
-        mapType: MapType.normal,
-        initialCameraPosition: widget.initialPosition,
-        myLocationEnabled: true,
-        myLocationButtonEnabled: true,
-        circles: Set.from([widget.circle]),
-        markers: widget.markers,
-
-        // 선택시 마커 변경
-        onTap: setMarkerOnPos,
-      ),
-    );
-  }
-
   // tap 한곳에 마커 추가
-  LatLng setMarkerOnPos(LatLng latlang) {
+  void setMarkerOnPos(LatLng latlang) {
     setState(() {
+      // Position과 마커 갱신
+      targetLatLng = latlang;
+      _cameraPosition = CameraPosition(
+        target: targetLatLng,
+        zoom: _zoom, // 카메라 확대 정도
+      );
+      circleWithIn = Circle(
+          circleId: CircleId('circleWithIn'), // circle간 식별자
+          center: targetLatLng,
+          fillColor: Colors.red.withOpacity(0.5), // 원 색
+          strokeColor: Colors.red, // 테두리 색
+          strokeWidth: 1, // 테두리 두께
+          radius: distRadius);
+      circleNotWithIn = Circle(
+          circleId: CircleId('circleNotWithIn'), // circle간 식별자
+          center: targetLatLng,
+          fillColor: Colors.blue.withOpacity(0.5), // 원 색
+          strokeColor: Colors.blue, // 테두리 색
+          strokeWidth: 1, // 테두리 두께
+          radius: distRadius);
 
-      if(widget.markers.isNotEmpty)
-      {
-        widget.markers.clear();
+      // 마커 갱신
+      if (_markers.isNotEmpty) {
+        _markers.clear();
       }
-      widget.markers.add(Marker(
+      _markers.add(Marker(
         draggable: true,
         markerId: MarkerId('target'),
         position: latlang,
@@ -292,6 +297,11 @@ class _CustomGoogleMapState extends State<_CustomGoogleMap> {
       ));
     });
 
-    return latlang;
+    // return latlang;
+  }
+
+  // 구글 맵 생성시 GoogleMapController를 state로 저장
+  onMapCreated(GoogleMapController controller) {
+    mapController = controller;
   }
 }
