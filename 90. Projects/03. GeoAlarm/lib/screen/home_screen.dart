0de9386eb9c1ed8dea2cd240b220340f533c75da
k-min9 <mingu4969@gmail.com>
2023-02-05 19:13:32 +0900

    230205 flutter admob 광고 게재 예시

--- 90. Projects/03. GeoAlarm/lib/screen/home_screen.dart
+++ 90. Projects/03. GeoAlarm/lib/screen/home_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:typed_data';
+
+import 'package:admob_flutter/admob_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:geo_alarm/location_controller.dart';
@@ -5,8 +8,8 @@ import 'package:geo_alarm/location_search_dialog.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:location/location.dart';
-import 'package:vibration/vibration.dart';
 import 'package:geo_alarm/main.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -16,10 +19,25 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
+
+
 class _HomeScreenState extends State<HomeScreen> {
   // 구글 맵 컨트롤러 최상단에 생성하고 이벤트 제어
   GoogleMapController? mapController;
   LocationController? locationController;
+  // BannerAd? myBanner;
+  //
+  // @override
+  // void initState() {
+  //   myBanner = BannerAd(
+  //       size: AdSize.banner,
+  //       adUnitId: 'ca-app-pub-8480822096988962/6768547151',
+  //       listener: BannerAdListener(),
+  //       request: AdRequest()
+  //   );
+  //   myBanner!.load();
+  //   super.initState();
+  // }
 
   final Location location = new Location();
   static late LocationData myLocationData;
@@ -46,6 +64,9 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // final AdWidget adWidget = AdWidget(ad:myBanner!);
+    // final Container adContainer = Container(child: adWidget, width: myBanner!.size.width.toDouble(), height: myBanner!.size.height.toDouble());
+
     return GetBuilder<LocationController>(builder: (locationController) {
       return Scaffold(
           appBar: AppBar(
@@ -104,6 +125,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     setLocationController(locationController);
                     return StreamBuilder<LocationData>(
                       stream: location.onLocationChanged, // position 값을 받아 snapshot에 넣음
+                      // 위치 확인 중 UI 관련 로직 : 백그라운드 작동 안 함
                       builder: (context, snapshot) {
                         WidgetsBinding.instance.addPostFrameCallback((_) => onAfterStreamBuild(context)); // StreamBuilder 중 setState관련 문제 해결 
                         // 범위 안에 있나 체크
@@ -114,7 +136,6 @@ class _HomeScreenState extends State<HomeScreen> {
                           // 검색으로 타겟을 잡은 경우, 타겟 지점 갱신
                           if(locationController.isSearched == true) {
                             targetLatLng = locationController.pickLatLng;
-                            // setMarkerOnPos(targetLatLng);
                           }
 
                           final start = snapshot.data!; // 내 위치
@@ -149,34 +170,6 @@ class _HomeScreenState extends State<HomeScreen> {
                               );
                             }
                           }
-
-                          final distance = Geolocator.distanceBetween(
-                            start.latitude!,
-                            start.longitude!,
-                            end.latitude,
-                            end.longitude,
-                          );
-
-
-                          if (distance < distRadius) {
-                            isWithinRange = true;
-                          }
-
-                          // 알람이 켜져있으면 발동 (일단 test)
-                          if (isWithinRange == true && isAlarmOn == true) {
-                            isAlarmOn = false;
-                            _circles.clear();
-                            _circles.add(Circle(
-                                circleId: CircleId('circleNormal'),
-                                // circle간 식별자
-                                center: targetLatLng,
-                                fillColor: Colors.blue.withOpacity(0.5), // 원 색
-                                strokeColor: Colors.blue, // 테두리 색
-                                strokeWidth: 1,
-                                // 테두리 두께
-                                radius: distRadius));
-                            fireAlarm();
-                          }
                         }
 
                         return Column(
@@ -197,14 +190,34 @@ class _HomeScreenState extends State<HomeScreen> {
                             ),
                             // 버튼 입력
                             Expanded(
-                                child: Switch(
-                                  value: isAlarmOn,
-                                  onChanged: (value) {
-                                    setState(() {
-                                      isAlarmOn = value;
-                                      isAlarmStateChanged = true; // flag
-                                    });
-                                  },
+                                child: Column(
+                                  children: [
+                                    Switch(
+                                      value: isAlarmOn,
+                                      onChanged: (value) {
+                                        setState(() {
+                                          isAlarmOn = value;
+                                          isAlarmStateChanged = true; // flag
+                                        });
+                                      },
+                                    ),
+                                    SizedBox(width: 5),
+                                    // Container(
+                                    //     height: 50,
+                                    //     width: 200,
+                                    //     child: adContainer
+                                    // ),
+                                    Center(
+                                      child: Container(
+                                        child: AdmobBanner(
+                                          adUnitId: "ca-app-pub-8480822096988962/6768547151",
+                                          adSize: AdmobBannerSize.BANNER,
+                                          onBannerCreated: (AdmobBannerController controller) {},
+                                        ),
+                                        color: Colors.red,
+                                      ),
+                                    )
+                                  ],
                                 )
                             ),
                           ],
@@ -224,7 +237,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   right: 20,
                   child: GestureDetector(
                       onTap: () {
-                        Vibration.vibrate();
+                        // Vibration.vibrate();
                         Get.dialog(
                             LocationSearchDialog(mapController: mapController));
                       },
@@ -271,7 +284,9 @@ class _HomeScreenState extends State<HomeScreen> {
     if(locationController?.isSearched == true) {
       setMarkerOnPos(targetLatLng);
     // 알람 관련 체크 : 원 갱신
-    } else if(isAlarmStateChanged == true) {
+    }
+
+    if(isAlarmStateChanged == true) {
       isAlarmStateChanged = false;
       if (_circles.isNotEmpty) {
         _circles.clear();
@@ -324,6 +339,34 @@ class _HomeScreenState extends State<HomeScreen> {
       return '앱의 위치 권한을 세팅에서 허가해주세요.';
     }
 
+    await location.enableBackgroundMode(enable: true);
+    // 위치 확인 중 알람 관련 로직은 백그라운드+포그라운드 양쪽에서 다 작동하는 여기서.
+    location.onLocationChanged.listen((LocationData currentLocation) {
+      // 알람이 켜져있을때
+      if (isAlarmOn == true) {
+        final start = currentLocation; // 내 위치
+        final end = targetLatLng; // 타겟 장소, 백그라운드 중에 바뀌지 않을 것이라 믿고
+        
+        bool isWithinRange = false;
+
+        final distance = Geolocator.distanceBetween(
+          start.latitude!,
+          start.longitude!,
+          end.latitude,
+          end.longitude,
+        );
+        
+        if (distance < distRadius) {
+          isWithinRange = true;
+        }
+
+        if (isWithinRange == true) {
+          isAlarmOn = false;
+          isAlarmStateChanged = true;
+          fireAlarm();
+        }
+      }
+    });
     return '위치 권한이 허가 되었습니다.';
   }
 
@@ -389,20 +432,31 @@ class _HomeScreenState extends State<HomeScreen> {
   void fireAlarm() async {
     print('Test Fired');
 
+    // var vibrationPattern = new Int64List(8);
+    // vibrationPattern[0] = 500;
+    // vibrationPattern[1] = 1000;
+    // vibrationPattern[2] = 500;
+    // vibrationPattern[3] = 2000;
+    // vibrationPattern[4] = 500;
+    // vibrationPattern[5] = 3000;
+    // vibrationPattern[6] = 500;
+    // vibrationPattern[7] = 500;
+
     var androidPlatformChannelSpecifics = AndroidNotificationDetails(
       'alarm_notif',
       'alarm_notif',
       channelDescription: 'Channel for Alarm notification',
-      icon: 'logo',
+      // icon: 'app_icon',
       // sound: RawResourceAndroidNotificationSound('bgm1'),
-      largeIcon: DrawableResourceAndroidBitmap('logo'),
+      // largeIcon: DrawableResourceAndroidBitmap('logo'),
+      // vibrationPattern: vibrationPattern,
     );
 
     var platformChannelSpecifics = NotificationDetails(
         android: androidPlatformChannelSpecifics
     );
 
-    await flutterLocalNotificationsPlugin.show(0, 'title', 'body', platformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(0, '도착하였습니다.', '설정 : 목적지까지 100m', platformChannelSpecifics);
 
   }
 }
