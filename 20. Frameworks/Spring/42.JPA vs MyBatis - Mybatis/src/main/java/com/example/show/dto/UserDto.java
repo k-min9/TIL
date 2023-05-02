eea5715409897dca2fd93bff9a76dcea3601482f
k-min9 <mingu4969@gmail.com>
2023-05-03 00:31:38 +0900

    230503 mybatis dto 구성

---
+++ 20. Frameworks/Spring/42.JPA vs MyBatis - Mybatis/src/main/java/com/example/show/dto/UserDto.java
@@ -0,0 +1,12 @@
+package com.example.show.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UserDto {
+  private String user_no;
+  private String name;
+}
+
