26ed5f9fbaab472cdce361e9bb1b1f871a78a6e0
k-min9 <mingu4969@gmail.com>
2023-05-30 00:04:01 +0900

    230530 projects VITS Inference Server before Flask

---
+++ 90. Projects/04. VITS/InferenceServer/app.py
@@ -0,0 +1,38 @@
+import torch
+
+import commons
+import utils
+from models import SynthesizerTrn
+from text.symbols import symbols
+from text import text_to_sequence
+
+from scipy.io.wavfile import write
+
+
+def get_text(text, hps):
+    text_norm = text_to_sequence(text, hps.data.text_cleaners)
+    if hps.data.add_blank:
+        text_norm = commons.intersperse(text_norm, 0)
+    text_norm = torch.LongTensor(text_norm)
+    return text_norm
+
+hps = utils.get_hparams_from_file("./data/config.json")
+
+net_g = SynthesizerTrn(
+    len(symbols),
+    hps.data.filter_length // 2 + 1,
+    hps.train.segment_size // hps.data.hop_length,
+    **hps.model).cuda()
+_ = net_g.eval()
+
+_ = utils.load_checkpoint("./data/G_1780.pth", net_g, None)
+
+# stn_tst = get_text("こんにちは", hps)
+stn_tst = get_text("すみません…できればでいいのですが、わたしについできてください。", hps)
+
+with torch.no_grad():
+    x_tst = stn_tst.cuda().unsqueeze(0)
+    x_tst_lengths = torch.LongTensor([stn_tst.size(0)]).cuda()
+    audio = net_g.infer(x_tst, x_tst_lengths, noise_scale=.667, noise_scale_w=0.8, length_scale=1)[0][0,0].data.cpu().float().numpy()
+
+write('./output.wav', hps.data.sampling_rate, audio)
