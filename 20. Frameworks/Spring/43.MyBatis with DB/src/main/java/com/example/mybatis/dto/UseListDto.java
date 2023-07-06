45b5e63f8f6a126c709deadf1b16852ce86c4bc3
k-min9 <mingu4969@gmail.com>
2023-07-07 00:20:06 +0900

    230703 spring mybatis 결과 중 리스트 반환 End

--- 20. Frameworks/Spring/43.MyBatis with DB/src/main/java/com/example/mybatis/dto/UseListDto.java
+++ 20. Frameworks/Spring/43.MyBatis with DB/src/main/java/com/example/mybatis/dto/UseListDto.java
@@ -8,13 +8,13 @@ import java.util.List;
 @Getter @Setter
 public class UseListDto {
 
-    private String use_no = "use_no_1";
-    private int use_distance = 10;
-    private int use_time = 100;
-    private String use_start_dt = "2023-01-01 11:00:00";
-    private String use_end_dt = "2023-01-23 23:00:00";
-    private String pay_datetime = "2023-01-01";
-    private int card_pay = 1;
-    private int point_pay = 2;
+    private String use_no;
+    private int use_distance;
+    private int use_time;
+    private String use_start_dt;
+    private String use_end_dt;
+    private String pay_datetime;
+    private int card_pay;
+    private int point_pay;
     private List<String> pay_method;
 }
